4a0e902e8e98a134581b85a2b7e9637e7876fa0b
kapilkataria-nix <kapilkataria245@gmail.com>
2023-06-30 22:24:36 +0530

    Done!

--- Python_Multi_Clipboard.py
+++ Python_Multi_Clipboard.py
@@ -1,14 +1,14 @@
 import sys 
 import clipboard
 import json
-
+#key : --
 Saved_data  = 'Clipboard.json'
 
 #save data the json file, If data is not present add it
 def Save(filepath,data):
     with open(filepath,'w') as f:
         json.dump(data, f)
-    
+
 def Load(filepath):
     try:
         with open(filepath,'r') as f:
@@ -16,7 +16,7 @@ def Load(filepath):
             return data
     except:
         return {}
-           
+
 if len(sys.argv) == 2:
     command = sys.argv[1]
     data = Load(Saved_data)
@@ -25,14 +25,21 @@ if len(sys.argv) == 2:
         key  = input("Enter a key: ")
         data[key] = clipboard.paste()
         Save(Saved_data, data)
-        print('Data Saved')
+        print('Data Saved!')
 
     elif command == "Load":
-        key = input('Enter key')
+        key = input('Enter key: ')
         if key in data:
                 clipboard.copy(data[key])
         else:
             print('Key does not exist')
+    
+    elif command == "Change":
+        key = input('Enter previous key: ')
+        change = input("Enter the new key: ")
+        data[change] = data[key]
+        del data[key]
+        Save(Saved_data, data)
 
     elif command == "List":
         print(data)
