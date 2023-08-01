7578e5e71d44865e234d8b6e2728860bd60e9322
kapilkataria-nix <kapilkataria245@gmail.com>
2023-08-01 16:29:32 +0530

    his

--- weather_fetcher/weather.py
+++ weather_fetcher/weather.py
@@ -1,5 +1,5 @@
-import requests
-api_key = '4d0fcc8ab492f741ae9bda199634c5cb'
+import requests,os
+api_key = os.getenv('API-KEY')
 base_url = 'http://api.openweathermap.org/data/2.5/weather'
 
 query = input('Enter the city: ')
@@ -8,8 +8,8 @@ response = requests.get(requests_url)
 
 if response.status_code == 200:
     data = response.json()
-    weather = data[weather]
-    temperature = data[temperature]
+    weather = data['weather']
+    temperature = data['main']['temp']
     print(f'Weather: {weather}')
     print(f'Temperature: {temperature}')
 else:
